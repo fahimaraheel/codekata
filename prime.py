435cf4b4035837fa572faad276e28a147d6c59db
fahimaraheel <47441145+fahimaraheel@users.noreply.github.com>
2019-02-19 08:58:32 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,9 +1,9 @@
-a=int(input())
-k=0
-for i in range(2,a//2+1):
-    if(a%i==0):
-        k=k+1
-if(k<=0):
+b=int(input())
+l=0
+for i in range(2,b//2+1):
+    if(b%i==0):
+        l=l+1
+if(l<=0):
     print("yes")
 else:
     print("no")
